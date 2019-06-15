9f561930b4c56e68920e63fd562b6cd645fc933a
WillX <44008280+willxudallas@users.noreply.github.com>
2019-06-15 15:00:32 -0500

    Add files via upload
    How to create linked list class and add basic add, display, remove function

---
+++ Linked list.py
@@ -0,0 +1,76 @@
+
+class node:
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = node()
+
+    def append(self,data):
+        new_node = node(data)
+        cur =self.head
+        while cur.next!=None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next!=None:
+            total+=1
+            cur = cur.next
+        return total
+
+    def display(self):
+        elems = []
+        cur_node = self.head
+        while cur_node.next!=None:
+            cur_node=cur_node.next
+            elems.append(cur_node.data)
+        print(elems)
+
+    def get(self,index):
+        if index >=self.length():
+            print("ERROR: 'Get' Index out of range! ")
+            return None
+        cur_idx=0
+        cur_node=self.head
+        while True:
+            cur_node=cur_node.next
+            if cur_idx==index:
+                return cur_node.data
+            else:
+                cur_idx+=1
+
+    def erase(self,index):
+        if index>=self.length():
+            print("ERROR: 'Erase' Index out of range")
+            return None
+        else:
+            cur_idx = 0
+            cur_node = self.head
+            while True:
+                last_node = cur_node
+                cur_node = cur_node.next
+                if cur_idx==index:
+                    last_node.next = cur_node.next
+                    return
+                cur_idx+=1
+
+my_list = linked_list()
+
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+
+my_list.display()
+
+print("element at 2nd index : %d" % my_list.get(2))
+
+my_list.erase(1)
+
+my_list.display()
